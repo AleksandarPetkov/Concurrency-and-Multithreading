d8db8c29a419aa4fd598c16a271392d7a076fe10
unknown <a.petkovpetkov@gmail.com>
2021-09-04 10:22:37 +0300

    Adding Writer Reader Consumer Producer issue

---
+++ src/consumer/producer/Start.java
@@ -0,0 +1,14 @@
+package consumer.producer;
+
+public class Start {
+    public static void main(String[] args) {
+        //Shared message object between Reader and Writer threads.
+        Message message = new Message();
+
+        Thread writerThread = new Thread(new Writer(message));
+        Thread readerThread = new Thread(new Reader(message));
+
+        writerThread.start();
+        readerThread.start();
+    }
+}
--- src/messages/Main.java
+++ src/messages/Main.java
@@ -2,6 +2,8 @@ package messages;
 
 public class Main {
     public static void main(String[] args) {
-
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
     }
 }
